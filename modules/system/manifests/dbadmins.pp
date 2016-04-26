e3e753dddb4266995d012a658bcfa914a4bf8b79
joel <joel@puppetlabs.vm>
2016-04-26 17:35:00 +0000

    Add system module 3.1

---
+++ modules/system/manifests/dbadmins.pp
@@ -0,0 +1,13 @@
+class system::dbadmins {
+  require mysql::server
+  Mysql_user {
+    ensure               => present,
+    max_queries_per_hour => 600,
+  }
+
+  mysql_user { 'zack@localhost':
+    max_queries_per_hour => 1200,
+  }
+
+  mysql_user { ['monica@localhost','ralph@localhost','brad@localhost','luke@localhost']: }
+}
