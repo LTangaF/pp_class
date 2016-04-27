c690d503738a62b4dea925709bbb515d5538ff73
joel <joel@puppetlabs.vm>
2016-04-27 15:47:39 +0000

    Fixed default_realm fact

--- modules/kerberos/lib/facter/default_realm.rb
+++ modules/kerberos/lib/facter/default_realm.rb
@@ -1,7 +1,9 @@
 # default_realm.rb
 require 'augeas'
 Facter.add('default_realm') do
-  Augeas::open do |aug|
-    aug.get('/files/etc/krb5.conf/libdefaults/default_realm')
+  setcode do
+    Augeas::open do |aug|
+      aug.get('/files/etc/krb5.conf/libdefaults/default_realm')
+    end
   end
 end
